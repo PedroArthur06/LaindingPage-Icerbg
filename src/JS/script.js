0f89e9e9e787aaaa91278d4774a54dcb0da88990
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-05-01 19:23:02 -0400

    java script implementado ao clicar no botão do formulário

---
+++ src/JS/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form-contato");
+
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const nome = document.getElementById("nome").value;
+      const email = document.getElementById("email").value;
+      const telefone = document.getElementById("telefone").value;
+      const servico = document.getElementById("servico").value;
+
+      const mensagem = `Olá! Vi seu perfil no Instagram e gostaria de saber mais sobre os serviços de ar-condicionado.%0A
+Nome: ${nome}%0A
+E-mail: ${email}%0A
+Telefone: ${telefone}%0A
+Serviço: ${servico}`;
+
+      const url = `https://wa.me/65981325445?text=${mensagem}`;
+      window.open(url, "_blank");
+    });
+  }
+});
