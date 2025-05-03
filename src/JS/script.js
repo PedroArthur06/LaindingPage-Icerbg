0fa2a42c9d01f2c20b5e010f8d1f7e82828d18c9
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-05-03 15:32:48 -0400

    swiper adicionado ao JS

--- src/JS/script.js
+++ src/JS/script.js
@@ -20,6 +20,29 @@ Serviço: ${servico}`;
       window.open(url, "_blank");
     });
   }
+
+  // Initialize Swiper carousel for works section
+  const swiper = new Swiper('.works.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
+  });
 });
-
-
