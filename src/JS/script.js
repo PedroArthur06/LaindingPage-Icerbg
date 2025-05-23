81a2b0e64af99abae4376fd3332dbd56ea4f6156
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-05-23 16:55:13 -0400

    html e js atualizados

--- src/JS/script.js
+++ src/JS/script.js
@@ -21,29 +21,27 @@ Serviço: ${servico}`;
     });
   }
 
-  new Swiper('.card-wrapper', {
-    loop: true,
-    spaceBetween: 24,
-    slidesPerView: 'auto',
-    centeredSlides: true, 
+  new Swiper('.swiper', {
+      loop: true,
+      spaceBetween: 24,
+      slidesPerView: 'auto',
+      centeredSlides: true,
 
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true,
-    },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        0:    { slidesPerView: 1 },
+        768:  { slidesPerView: 2 },
+        1024: { slidesPerView: 3 },
+      },
+    });
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    
-    breakpoints: {
-      0:   { slidesPerView: 1 },
-      768: { slidesPerView: 2 },
-      1024:{ slidesPerView: 3 },
-    },
-  });
+    AOS.init();
 });
-
-AOS.init();
